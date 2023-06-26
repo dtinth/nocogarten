2c31725a8d51ca9a7fa99f118f31d71561c63503
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-26 23:47:55 +0700

    redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Home() {
   const redir = `${
-    process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+    process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
       : "http://localhost:3000"
   }/api/auth/callback`;
   const authHref = `https://creatorsgarten.org/auth/authorize?client_id=https://db.creatorsgarten.org&scope=openid+email&response_type=id_token&redirect_uri=${encodeURIComponent(
     redir
   )}`;
+  useEffect(() => {
+    location.replace(authHref);
+  }, [authHref]);
   return (
     <>
       <Head>
